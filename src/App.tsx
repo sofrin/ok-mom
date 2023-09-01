5d1a3a2b4cd9d68077f2673ed4b3fb725313edb1
sofrin <deadsalamanderninjatroll@gmail.com>
2023-09-02 01:36:30 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      123123123
+      123321
+      asddsa
+      asddsa
+      asdadsd
+      asd
+    </h1>
+  )
+}
+
+export default App
