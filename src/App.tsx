046d3b1fa2e288cde8bac8196eaf327ffdf6c409
sofrin <deadsalamanderninjatroll@gmail.com>
2023-09-03 10:02:50 +0700

    feat: added fixed landing component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
+import { Landing } from "./pages/Landing"
 
 function App() {
 
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-      123123123
-      123321
-      asddsa
-      asddsa
-      asdadsd
-      asd
-    </h1>
+    <Landing />
   )
 }
 
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+export const Landing = () => {
+  return <>
+    <header>
+      <div className="container">
+        <nav className="flex items-center justify-between px-10">
+          <a href="#" className="flex flex-row items-center p-2 gap-6 pr-12" ><img src='/Vector 4.svg' alt="logo" /> <span className="font-medium text-xl max-w-[100px]">Vigrebuha
+            Technology </span></a>
+          <ul className="flex gap-12 mr-auto ">
+            <li><a className=" font-medium text-xl   text-[#18A0FB]  " href="#">Features</a></li>
+            <li><a className=" font-medium text-xl   text-[#18A0FB] " href="#">Pricing</a></li>
+            <li><a className=" font-medium text-xl   text-[#18A0FB] " href="#">Community</a></li>
+            <li><a className=" font-medium text-xl   text-[#18A0FB] " href="#">Support</a></li>
+          </ul>
+          <div className="flex gap-3 "><button className="w-32 h-10 rounded-md border-[1px] border-[#18A0FB] text-[#18A0FB] text-center text-sm"> Log In</button>
+            <button className="w-32 h-10 rounded-md text-white bg-[#18A0FB] text-center text-sm">Register</button></div>
+        </nav>
+      </div>
+    </header>
+    <main className="bg-[#EAEBE4]">
+      <div className="container relative">
+        <img className="w-[1680px] h-[960px]" src="/hero.png" alt="heropng" />
+        <p className=" font-medium text-5xl  max-w-[600px] absolute top-[542px] left-[190px]" > The largest community of lazy parents</p>
+        <button className="w-32 h-10 rounded-md text-white bg-[#18A0FB] text-center text-sm absolute top-[680px] left-[190px]">Join Today</button></div>
+      <div className="contaiter flex p-20 justify-center items-center gap-20 ">
+        <img src="/about.png" alt="about" className="w-[750px] h-[440px] " />
+        <p className="font-normal text-5xl leading-[72px] "> Learn about all the features of the <span className="text-[#18A0FB]">ok mom</span> app </p>
+      </div>
+      <div className="container p-20">
+        <h2 className="w-[600px] text-5xl leading-[72px] tracking-tighter pb-32">Train your child like never before</h2>
+        <div className="flex flex-row flex-wrap justify-between">
+          <div className="flex flex-col max-w-2xl">
+            <h3 className="text-2xl font-bold pb-6">Sed ut perspiciatis</h3>
+            <p className="text-lg tracking-tighter pb-10"> Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.</p>
+          </div>
+          <div className="flex flex-col max-w-2xl">
+            <h3 className="text-2xl font-bold pb-6" >Lorem ipsum dolor</h3>
+            <p className="text-lg  tracking-tighter pb-10">Amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.</p>
+          </div>
+          <div className="flex flex-col max-w-2xl">
+            <h3 className="text-2xl font-bold pb-6">Nemo enim ipsam</h3>
+            <p className="text-lg tracking-tighter pb-10" >Consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam.</p>
+          </div>
+          <div className="flex flex-col max-w-2xl">
+            <h3 className="text-2xl font-bold pb-6">Tempor incididunt</h3>
+            <p className="text-lg tracking-tighter pb-10">Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora.</p>
+          </div>
+        </div>
+      </div>
+      <div className="contaiter flex p-20  items-center justify-center gap-12">
+        <div className="w-[1000px]">
+          <p className="font-normal text-4xl leading-[50px] tracking-[-0.54px] pb-8"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore </p>
+          <div className="flex flex-row gap-6 items-center">
+            <img className="h-[44px] w-[44px] rounded-[44px]" src="/avatar.png" alt="avatar" />
+            <p className="text-lg">Happy lazy parent</p>
+          </div>
+        </div>
+        <img src="/feature.jpg" alt="about" className="w-[500px] h-[470px] object-cover" />
+
+      </div>
+      <div className="contaiter flex p-20  items-center justify-between  pr-56">
+        <p className="font-normal text-4xl leading-[50px] tracking-[-0.54px] pb-8 w-[900px]"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore </p>
+        <button className="w-48 h-12 rounded-md text-white bg-[#18A0FB] text-center text-base">Join Today</button>
+      </div>
+    </main>
+    <footer className="bg-[#EAEBE4]">
+      <nav className="flex items-center  justify-between px-10 ">
+        <a href="#" className="flex flex-row items-center p-2 gap-6 pl-14" ><img src='/Vector 4.svg' alt="logo" /> <span className="font-medium text-xl max-w-[100px]">Vigrebuha
+          Technology </span></a>
+        <ul className="flex gap-28  justify-center items-center flex-1">
+          <li><a className=" font-medium text-xl   text-black  " href="#">Mobile app</a></li>
+          <li><a className=" font-medium text-xl   text-black " href="#">Community</a></li>
+          <li><a className=" font-medium text-xl   text-black " href="#">Company</a></li>
+
+        </ul>
+        <p className="opacity-80 w-[240px] "> © VigrebuhaTechnology, Inc. 2023. We love our users!</p>
+      </nav>
+    </footer>
+
+  </>;
+};
