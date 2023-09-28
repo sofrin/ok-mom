7a71c49b418095ca04a6ec68698e464db10fd301
sofrin <deadsalamanderninjatroll@gmail.com>
2023-09-28 23:04:35 +0700

    fixed NotFound bg

--- src/widgets/NotFound/NotFound.tsx
+++ src/widgets/NotFound/NotFound.tsx
@@ -1,14 +1,27 @@
+import { Grid, Paper } from '@mui/material';
 import React from 'react';
 
 export const NotFound = () => {
 	return (
-		<div className='container'>
-			<h1>
-				<span>:( </span>
-				<br />
-				Ничего не найдено
-			</h1>
-			<p>К сожалению данная страница отсуствует.</p>
-		</div>
+		<Grid
+			item
+			sx={{ marginLeft: 'auto', marginRight: 'auto', width: 'fit-content' }}
+		>
+			<Paper
+				sx={{
+					p: 3,
+					display: 'flex',
+					flexDirection: 'column',
+					width: 'fit-content',
+				}}
+			>
+				<h1>
+					<span>😔 </span>
+					<br />
+					Ничего не найдено
+				</h1>
+				<p>К сожалению данная страница отсуствует.</p>
+			</Paper>
+		</Grid>
 	);
 };
