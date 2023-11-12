0016ce1ecc426eb1c40be97b881f27c8418ea84a
Sofrin <85531415+sofrin@users.noreply.github.com>
2023-11-12 15:27:34 +0700

    add child history page

---
+++ src/widgets/ChildViewHistory/ChildVIewHistory.tsx
@@ -0,0 +1,85 @@
+import { useAutoAnimate } from '@formkit/auto-animate/react';
+import { Box, Divider, ListItem, Typography } from '@mui/material';
+import List from '@mui/material/List';
+import {
+	childArchievedTasks,
+	childCompletedTasks,
+} from 'entities/CardTask/model/filterTasks';
+import { getTasksThunk, selectTasks } from 'entities/CardTask/model/taskSlice';
+import { HistoryChildCard } from 'entities/HistoryChildCard/HistoryChildCard';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from 'shared/model/hooks';
+import { taskSchema } from 'shared/types';
+
+export const ChildVIewHistory = ({ child }: { child: string }) => {
+	const dispatch = useAppDispatch();
+	useEffect(() => {
+		const fetchTasks = async () => {
+			dispatch(getTasksThunk('')).unwrap();
+		};
+		fetchTasks();
+	}, [dispatch]);
+	const tasks = useAppSelector(selectTasks);
+	const archievedTasks = childArchievedTasks(tasks, child);
+	const completedTasks = childCompletedTasks(tasks, child);
+	const [parent] = useAutoAnimate();
+
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				flexDirection: 'row',
+				justifyContent: 'center',
+				width: 'auto',
+				height: '700px',
+				overflowY: 'auto',
+			}}
+		>
+			<List
+				ref={parent}
+				disablePadding
+			>
+				<Typography
+					variant='h5'
+					sx={{ marginBottom: '10px' }}
+				>
+					Задания на проверке:
+				</Typography>
+
+				{completedTasks.map((obj: taskSchema) => (
+					<ListItem
+						disablePadding
+						key={obj.id}
+						sx={{ width: 400 }}
+					>
+						<HistoryChildCard {...obj} />
+					</ListItem>
+				))}
+			</List>
+
+			<Box sx={{ height: 'auto', margin: '0 30px' }}>
+				<Divider orientation='vertical' />
+			</Box>
+			<List
+				ref={parent}
+				disablePadding
+			>
+				<Typography
+					variant='h5'
+					sx={{ marginBottom: '10px' }}
+				>
+					Подтвержденные задания:
+				</Typography>
+				{archievedTasks.map((obj: taskSchema) => (
+					<ListItem
+						disablePadding
+						key={obj.id}
+						sx={{ width: 400 }}
+					>
+						<HistoryChildCard {...obj} />
+					</ListItem>
+				))}
+			</List>
+		</Box>
+	);
+};
